90e70f6c5ba771aeaf2cd31b52f089d86f02e1af
chanakarnkingkaew <chanakarnkingkaew@localhost>
2022-08-10 14:38:01 +0900

    Second Commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chanakarnkingkaew
+ */
+public class Person implements Comparable<Person> {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean equals(Person that) {
+        return this.name.equals(that.name);
+    }
+
+    @Override
+    public int compareTo(Person that) {
+        return this.name.compareTo(that.name); // using String compareTo
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + '}';
+    }
+}
